7378e37c4f04bc82f34c3ddf47141ad9dd53bcce
Lyla Lynn <lyladlynn@gmail.com>
2025-03-25 20:56:43 -0500

    added data.js file with puppy data. created useState hook and mapped over the array in the div.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-  
-
+  const [puppies, setPuppies] = useState(puppyList);  
+console.log(puppies);
   return (
     <div>
-      
+      {
+        puppies.map((puppy) => {
+          return <p>{puppy.name}</p>
+        })
+      }
     </div>
   )
 }
